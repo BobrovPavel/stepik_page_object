6922519120123b4715d891b15fc0a1060f7be465
BobrovPavel <pavel@getquin.com>
2024-04-08 16:13:27 +0300

    add assertion error message for should_be_message_that_basket_is_empty

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -5,7 +5,9 @@ from .locators import BasketPageLocators
 class BasketPage(BasePage):
 
     def should_be_empty_basket(self):
-        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEM)
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEM), "The basket doesn't seem to be empty"
 
     def should_be_message_that_basket_is_empty(self):
-        assert self.is_element_present(*BasketPageLocators.BASKET_IS_EMPTY_MESSAGE)
+        assert self.is_element_present(*BasketPageLocators.BASKET_IS_EMPTY_MESSAGE), f"Missing message that the basket is empty\n" \
+                                                                                     f"Element located at {BasketPageLocators.BASKET_IS_EMPTY_MESSAGE}" \
+                                                                                     f" was expected to be presented on the page"
