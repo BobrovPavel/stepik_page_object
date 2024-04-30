f8995b198fda7cc872698946231bbab42f5d3ceb
BobrovPavel <pavel@getquin.com>
2024-04-30 13:39:44 +0300

    stage

---
+++ qwe_conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome', help='Choose browser (chrome of firefox')
+    parser.addoption("--language", action='store', default="ru", help='Choose language')
+
+@pytest.fixture(scope="function")
+def browser(request):
+    print("\nstart browser for test..")
+    user_language = request.config.getoption("language")
+    browser_name = request.config.getoption('browser_name')
+
+    if browser_name == 'chrome':
+        options = webdriver.ChromeOptions()
+        browser = webdriver.Remote(options=options)
+        # options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        # options.set_capability("browserVersion", "124")
+        # options.set_capability("platformName", "Windows 11")
+
+    elif browser_name == 'firefox':
+        options = OptionsFirefox()
+        options.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(options=options)
+    else:
+        raise pytest.UsageError("browser_name should be 'chrome' of 'firefox'")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
